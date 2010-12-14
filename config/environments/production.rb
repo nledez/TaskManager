8089baed6eef969ab5479fb4b582eac875ae2d43
Nicolas Ledez <github@ledez.net>
2010-12-14 23:16:09 +0100

    Installation de Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ TaskManager::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'mytaskmanager.heroku.com' }
 end
