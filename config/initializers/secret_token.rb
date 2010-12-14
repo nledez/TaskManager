c3c65303ca29ac069951e80aeb3eb327ac184ec2
Nicolas Ledez <github@ledez.net>
2010-12-14 22:58:35 +0100

    Rails tout frais

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = '3e33eda3548d26b4be75c32cbceef0dd0d4f7539979e8a8cd0fcaaf1f501d33bdaa6a9efa79f3805f88d4537ba569a1261fc2427ca4c471cf1ad52ab17e1ea41'
