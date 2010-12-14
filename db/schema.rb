b134ab826517843c410e39c16bd44aec4af51222
Nicolas Ledez <github@ledez.net>
2010-12-14 23:26:34 +0100

    Creation de la migration pour associer un user à un projet

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101214221639) do
+ActiveRecord::Schema.define(:version => 20101214222532) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
